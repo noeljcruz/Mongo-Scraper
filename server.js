e0d29df7db8d2c308af09e1bb841efbb7bb5988a
Noel Cruz <noel.j.cruz@outlook.com>
2019-10-23 21:31:21 -0700

    Added additional dependencies and modified server.js file.

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ var mongoose = require("mongoose");
 // It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
+var logger = require("morgan");
+
+var PORT = process.env.PORT || 3000;
 
 // Require all models
 var db = require("./models");
-
-var PORT = 3000;
 
 // Initialize Express
 var app = express();
@@ -19,14 +20,21 @@ var app = express();
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static("./public"));
+
+// Handlebars
+var exphb = require("express-handlebars");
+app.engine("handlebars", exphb({ defaultLayout: "man" }));
+app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/newsscraper", { useNewUrlParser: true });
 
 // Start the server
 app.listen(PORT, function () {
